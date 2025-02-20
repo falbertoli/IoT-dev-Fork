92127ee2d223d89d10be18bdd8d8ce4ad6cf44c7
falbertoli <falbertoli3@gatech.edu>
2025-02-20 00:00:35 -0500

    feature-navigation-bar: added a navidation bar for the different routers

--- iot-frontend/src/App.vue
+++ iot-frontend/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/maps">Map and Charts View</router-link> |
+      <router-link to="/delta">Delta View</router-link>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -15,5 +20,18 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
   margin-top: 40px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
